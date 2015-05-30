28589c39e860660636ad8b98e7dc1db10099462e
Roly Vicaria <rolyv19@gmail.com>
2015-05-30 09:55:08 -0400

    Adding -y flag for adding apt repository

--- setup.sh
+++ setup.sh
@@ -2,6 +2,6 @@
 # Simple setup.sh
 
 sudo apt-get install -y software-properties-common
-sudo apt-add-repository ppa:ansible/ansible
+sudo apt-add-repository -y ppa:ansible/ansible
 sudo apt-get update
 sudo apt-get install -y ansible
