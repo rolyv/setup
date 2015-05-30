831ded3598415545fc8399d9cb8061122ed14154
Roly Vicaria <rolyv19@gmail.com>
2015-05-30 09:45:55 -0400

    Adding -y flag to package installs

--- setup.sh
+++ setup.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 # Simple setup.sh
 
-sudo apt-get install software-properties-common
+sudo apt-get install -y software-properties-common
 sudo apt-add-repository ppa:ansible/ansible
 sudo apt-get update
-sudo apt-get install ansible
-
+sudo apt-get install -y ansible
