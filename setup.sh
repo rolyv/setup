d1d078805a756bf57ad186ccfa0699b6007aa72f
Roly Vicaria <rolyv19@gmail.com>
2015-05-30 09:39:20 -0400

    Modified setup.sh to start by installing latest version of ansible

--- setup.sh
+++ setup.sh
@@ -1,31 +1,8 @@
 #!/bin/bash
 # Simple setup.sh
 
-sudo apt-get install -y git
-sudo apt-get install -y curl
-
-# Install rlwrap to provide libreadline features
-sudo apt-get install -y rlwrap
-
-# Install emacs
-sudo apt-get install -y emacs
-
-# git pull and install dotfiles as well
-cd $HOME
-
-curl -o Anaconda.sh https://3230d63b5fc54e62148e-c95ac804525aac4b6dba79b00b39d1d3.ssl.cf1.rackcdn.com/Anaconda-2.2.0-Linux-x86_64.sh
-
-bash Anaconda.sh -b
-
-if [ -d ./dotfiles/ ]; then
-    mv dotfiles dotfiles.old
-fi
-if [ -d .emacs.d/ ]; then
-    mv .emacs.d .emacs.d~
-fi
-git clone https://github.com/rolyv/dotfiles.git
-ln -sb dotfiles/.bash_profile .
-ln -sb dotfiles/.bashrc .
-ln -sb dotfiles/.bashrc_custom .
-ln -sf dotfiles/.emacs.d .
+sudo apt-get install software-properties-common
+sudo apt-add-repository ppa:ansible/ansible
+sudo apt-get update
+sudo apt-get install ansible
 
